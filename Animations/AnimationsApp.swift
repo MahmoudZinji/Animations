09e2293b24743cd1bf6f3bad2e09900014e63ca1
Mahmoud Zinji <mahmoud.zinji@montymobile.com>
2021-06-11 11:51:27 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by User on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
